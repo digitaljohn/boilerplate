f3665e9ed7b839f600ac3a4c14305b5a6fe9c23f
DigitalJohn <digitaljohn@me.com>
2013-12-06 12:44:57 +0000

    Server start now using Async

--- server/server.js
+++ server/server.js
@@ -6,30 +6,38 @@
                            
 */
 
-var settings = require('./settings');
+var async = require('async');
 
+var settings = require('./settings');
 var uploader = require("./uploader");
 var db = require('./db');
 var mail = require('./mail');
 var httpserver = require("./httpserver");
 
 // Start the core server
-
-// Connect to the Dabase
-uploader.init( settings.amazon, function(err, res){
-    console.log("Amazon S3 Configured");
-
-    // Configure the Amazon S3 Uploader
-    db.init( settings.database, uploader, function(err, res){
-        console.log("Database Connected");
-
-        mail.init( settings.mail, function(err, res){
-            console.log("Mail Configured");
-
-            // Start the HTTP Server
-            httpserver.init(settings.http, db, mail, function(err, res){
-                console.log("--> Server up and running <--");
-            });
-        });
-    });
+async.series([
+    function(callback){
+        console.log('Init Uploader');
+        uploader.init( settings.amazon, callback);
+    },
+    function(callback){
+        console.log('Init Database');
+        db.init( settings.database, uploader, callback);
+    },
+    function(callback){
+        console.log('Init Mail');
+        mail.init( settings.mail, callback);
+    },
+    function(callback){
+        console.log('Init HTTP Server');
+        httpserver.init(settings.http, db, mail, callback);
+    }
+],
+function(err, results){
+    if(err){
+        console.log('error starting server!');
+    }else{
+        console.log('server started!');
+    }
 });
+
